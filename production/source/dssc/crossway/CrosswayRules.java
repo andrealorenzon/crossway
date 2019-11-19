ad22de5d7c66950b33deba992c681d27d4b48b89
Davide Scassola <projanko@gmail.com>
2019-11-19 10:43:35 +0100

    added first rule

--- production/source/dssc/crossway/CrosswayRules.java
+++ production/source/dssc/crossway/CrosswayRules.java
@@ -6,9 +6,14 @@ public class CrosswayRules extends Validator {
 
     }
 
+    private boolean noSuperposition(GoBoard board, Move m)  {
+        return board.getCellStatus(m.getX(), m.getY()) == Colors.EMPTY;
+    }
+
     @Override
-    public boolean validateMove(GoBoard board, Move m0) {
-        return true;
+    public boolean validateMove(GoBoard board, Move m) {
+        //check rule 1
+        return noSuperposition(board, m);
 
     }
 }
--- test/source/test/crossway/RulesTest.java
+++ test/source/test/crossway/RulesTest.java
@@ -3,18 +3,27 @@ package test.crossway;
 import dssc.crossway.*;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.validation.Validator;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RulesTest {
 
     @Test
-    void validateMoveTest()
+    void validateLegalMoveTest()
     {
         GameController gc = new GameController(new GoBoard(12), new CrosswayRules());
         gc.startGame();
         Move m0 = new Move(1,1, Colors.WHITE);
         assertTrue(gc.validateMove(m0));
+    }
+
+    @Test
+    void validateIllegalMoveTest() throws OutOfBoardException {
+        GameController gc = new GameController(new GoBoard(12), new CrosswayRules());
+        gc.startGame();
+        Move m0 = new Move(1,1, Colors.WHITE);
+        Move m1 = new Move(1,1, Colors.BLACK);
+        gc.placeStone(m0);
+        assertFalse(gc.validateMove(m1));
     }
 }
