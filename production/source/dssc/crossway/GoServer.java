d0b9fd04c9cac49246eb1aa6f9e37ff515c9f330
Andrea <andrelorenzon@gmail.com>
2019-11-20 10:24:02 +0100

    skeleton for server class

---
+++ production/source/dssc/crossway/GoServer.java
@@ -0,0 +1,7 @@
+package dssc.crossway;
+
+public class GoServer {
+    public GoServer(String ip, int port) {
+
+    }
+}
---
+++ test/source/test/crossway/NetworkingTest.java
@@ -0,0 +1,13 @@
+package test.crossway;
+
+import dssc.crossway.GoServer;
+import org.junit.jupiter.api.Test;
+
+public class NetworkingTest {
+    @Test
+    void ServerInitTest() {
+        String ip = "localhost";
+        int port = 10431;
+        GoServer s = new GoServer(ip, port)
+    }
+}
